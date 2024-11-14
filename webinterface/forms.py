44626e7938339f2ea11508e0147465116c50639f
tjurczyk <pyristfeuer@gmail.com>
2024-11-14 14:07:40 +0100

    Add initial documentation structure and web interface package

--- webinterface/forms.py
+++ webinterface/forms.py
@@ -1,3 +1,7 @@
+'''
+This module contains the FlaskForm classes used in the web interface of the typesetting tool.
+'''
+
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileField, FileAllowed, MultipleFileField
 from wtforms import SubmitField, FieldList, FormField, BooleanField, HiddenField, StringField, TextAreaField, IntegerField
@@ -7,8 +11,10 @@ class AffiliationForm(FlaskForm):
     """
     A form for capturing affiliation information.
 
-    Fields:
-        organization (StringField): An optional field to input the name of the organization.
+    Attributes
+    ----------
+    organization: StringField
+        An optional field to input the name of the organization.
     """
     organization = StringField('Organization', validators=[Optional()])
 
@@ -16,11 +22,16 @@ class AuthorForm(FlaskForm):
     """
     AuthorForm is a FlaskForm that collects information about an author.
 
-    Fields:
-        author_name (StringField): The name of the author. This field is optional.
-        affiliations (FieldList): A list of affiliations for the author. This field contains a minimum of 1 and a maximum of 3 AffiliationForm entries.
-        email (StringField): The email address of the author. This field is optional.
-        orcid (StringField): The ORCID identifier of the author. This field is optional.
+    Attributes
+    ----------
+    author_name : StringField
+        The name of the author. This field is optional.
+    affiliations : FieldList
+        A list of affiliations for the author, containing a minimum of 1 and a maximum of 3 AffiliationForm entries.
+    email : StringField
+        The email address of the author. This field is optional.
+    orcid : StringField
+        The ORCID identifier of the author. This field is optional.
     """
     author_name = StringField('Name', validators=[Optional()])
     affiliations = FieldList(FormField(AffiliationForm), min_entries=1, max_entries=3)
@@ -31,11 +42,20 @@ class FilterSelectionForm(FlaskForm):
     """
     FilterSelectionForm is a form used in a Flask web application to handle filter selections.
 
-    Fields:
-        selected (BooleanField): A boolean field that indicates whether any filters are selected. Defaults to "No filters found!".
-        file_name (HiddenField): A hidden field used to store the file name and pass this value to the MMM script. This is a workaround since passing the value via BooleanField was not possible due to HTML limitations with FieldList.
+    Attributes
+    ----------
+    selected: BooleanField
+        A boolean field that indicates whether any filters are selected. Defaults to "No filters found!".
 
-    Note:
+        .. :no-index:
+        
+    file_name: HiddenField
+        A hidden field used to store the file name and pass this value to the MMM script. This is a workaround since passing the value via BooleanField was not possible due to HTML limitations with FieldList.
+
+        .. :no-index:
+
+    Note
+    ----
         FieldList cannot enclose BooleanField or SubmitField instances due to HTML limitations.
         For more information, see: https://wtforms.readthedocs.io/en/2.3.x/fields/#wtforms.fields.FieldList
     """
@@ -50,18 +70,30 @@ class JournalDataForm(FlaskForm):
     """
     JournalDataForm is a FlaskForm for collecting and validating journal-related data.
 
-    Fields:
-        publisher_id (StringField): The ID of the publisher. Required.
-        eissn (StringField): The electronic ISSN of the journal. Required.
-        title (StringField): The title of the journal. Required.
-        publisher_name (StringField): The name of the publisher. Required.
-        publisher_loc (StringField): The location of the publisher. Required.
-        journal_link (StringField): The URL link to the journal. Required and must be a valid URL.
-        license_text (TextAreaField): The text of the license. Required.
-        license_type (StringField): The type of the license. Required.
-        license_link (StringField): The URL link to the license. Required and must be a valid URL.
-        copyright_holder (StringField): The holder of the copyright. Required.
-        submit (SubmitField): The submit button to update the form.
+    Attributes
+    ----------
+    publisher_id : StringField
+        The ID of the publisher. Required.
+    eissn : StringField
+        The electronic ISSN of the journal. Required.
+    title : StringField
+        The title of the journal. Required.
+    publisher_name : StringField
+        The name of the publisher. Required.
+    publisher_loc : StringField
+        The location of the publisher. Required.
+    journal_link : StringField
+        The URL link to the journal. Required and must be a valid URL.
+    license_text : TextAreaField
+        The text of the license. Required.
+    license_type : StringField
+        The type of the license. Required.
+    license_link : StringField
+        The URL link to the license. Required and must be a valid URL.
+    copyright_holder : StringField
+        The holder of the copyright. Required.
+    submit : SubmitField
+        The submit button to update the form.
     """
     # Journal section
     publisher_id = StringField('Publisher ID', validators=[DataRequired()])
@@ -86,31 +118,55 @@ class UploadForm(FlaskForm):
     """
     UploadForm is a FlaskForm that handles the upload and input of various files and metadata for typesetting.
 
-    Fields:
-        markdown (FileField): Required field for uploading a Markdown file. Only accepts files with extensions 'md' or 'markdown'.
-        metadata (FileField): Optional field for uploading a metadata file. Only accepts files with extensions 'yaml' or 'yml'.
-        bibtex (FileField): Optional field for uploading a BibTeX file. Only accepts files with extension 'bib'.
-        images (MultipleFileField): Optional field for uploading multiple image files. Only accepts files with extensions 'png', 'jpg', or 'jpeg'.
-        filter_selection (FieldList): Dynamic form field for selecting filters. Displays all filters in the filter folder.
-        file_name (StringField): Optional field for specifying individual file names.
-        pdf (BooleanField): Checkbox to select PDF output.
-        html (BooleanField): Checkbox to select HTML output.
-        jats (BooleanField): Checkbox to select JATS output.
-        latex (BooleanField): Checkbox to select LaTeX output.
-        manual_data_input (BooleanField): Checkbox to activate manual data input instead of uploading a metadata file.
-        submit (SubmitField): Button to submit the form and create files.
+    Attributes
+    ----------
+    markdown : FileField
+        Required field for uploading a Markdown file. Only accepts files with extensions 'md' or 'markdown'.
+    metadata : FileField
+        Optional field for uploading a metadata file. Only accepts files with extensions 'yaml' or 'yml'.
+    bibtex : FileField
+        Optional field for uploading a BibTeX file. Only accepts files with extension 'bib'.
+    images : MultipleFileField
+        Optional field for uploading multiple image files. Only accepts files with extensions 'png', 'jpg', or 'jpeg'.
+    filter_selection : FieldList
+        Dynamic form field for selecting filters. Displays all filters in the filter folder.
+    file_name : StringField
+        Optional field for specifying individual file names.
+    pdf : BooleanField
+        Checkbox to select PDF output.
+    html : BooleanField
+        Checkbox to select HTML output.
+    jats : BooleanField
+        Checkbox to select JATS output.
+    latex : BooleanField
+        Checkbox to select LaTeX output.
+    manual_data_input : BooleanField
+        Checkbox to activate manual data input instead of uploading a metadata file.
+    submit : SubmitField
+        Button to submit the form and create files.
 
-    Optional Metadata Fields (used if no metadata file is uploaded):
-        title (StringField): Optional field for the article title.
-        subtitle (StringField): Optional field for the article subtitle.
-        keywords (FieldList): Optional field for article keywords. Can have between 1 and 8 entries, each being a StringField.
-        abstract (TextAreaField): Optional field for the article abstract.
-        date (StringField): Optional field for the article year.
-        volume (IntegerField): Optional field for the article volume.
-        doi (StringField): Optional field for the article DOI.
-        author_short (StringField): Optional field for a short author description.
-        authors (FieldList): Optional field for author data. Can have between 1 and 20 entries, each being an AuthorForm.
+    Optional Metadata Fields (used if no metadata file is uploaded)
+    ---------------------------------------------------------------
+    title : StringField
+        Optional field for the article title.
+    subtitle : StringField
+        Optional field for the article subtitle.
+    keywords : FieldList
+        Optional field for article keywords. Can have between 1 and 8 entries, each being a StringField.
+    abstract : TextAreaField
+        Optional field for the article abstract.
+    date : StringField
+        Optional field for the article year.
+    volume : IntegerField
+        Optional field for the article volume.
+    doi : StringField
+        Optional field for the article DOI.
+    author_short : StringField
+        Optional field for a short author description.
+    authors : FieldList
+        Optional field for author data. Can have between 1 and 20 entries, each being an AuthorForm.
     """
+
     markdown = FileField('Markdown file (required)', validators=[FileRequired(), FileAllowed(["md", "markdown"])])
     metadata = FileField('Metadata file (optional)', validators=[FileAllowed(["yaml", "yml"])])
     bibtex = FileField('BibTeX file (optional)', validators=[FileAllowed(["bib"])])
