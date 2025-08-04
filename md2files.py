7b22d8e995ab7334bc47aa7b7fdf659e2364e02b
Felix Kopecky <kopeckyf@users.noreply.github.com>
2025-08-04 16:00:28 +0200

    First draft for new layout

--- md2files.py
+++ md2files.py
@@ -5,11 +5,10 @@ The container expects a yaml file with metadata and a markdown file
 Optionally, a BibTeX bibliography and individual Lua filters can be passed.
 '''
 
-# Version: 1.0.0
-# Date: 2024-10-04
-# by Thomas Jurczyk (c)
-
-import argparse, os, subprocess, shutil
+import argparse
+import os
+import subprocess
+import shutil
 
 def parse_arguments():
     '''Parse command line arguments.'''
@@ -40,6 +39,7 @@ def parse_arguments():
         "--filter", 
         "-f", 
         nargs="+", 
+        default=[],
         help="Pandoc Lua-filters to use. These filters must be stored in the" \
         " subfolder 'filter/'. Please provide the filter name WITH the " \
         "file extension."
@@ -78,6 +78,26 @@ def parse_arguments():
         help="The name of the output file. This is an optional argument. " \
         "If not provided, the name of the markdown file will be used."
         )
+    parser.add_argument(
+        "--layout",
+        type=str,
+        help="The layout in use"
+    )
+    parser.add_argument(
+        "--compounds",
+        action="store_true",
+        help="Whether to apply the filter for better hyphenation in compounds."
+    )
+    parser.add_argument(
+        "--parentheses",
+        action="store_true",
+        help="Whether a citation style with manual parentheses is to be used."
+    )
+    parser.add_argument(
+        "--widows",
+        action="store_true",
+        help="Automatically treat widows."
+    )
     return parser.parse_args()
 
 def logging(LOGFILE: str, result: subprocess.CompletedProcess) -> None:
@@ -107,6 +127,127 @@ def copy_files_to_app_dir() -> None:
     for filename in os.listdir("article/"):
         if filename.endswith((".png", ".jpg", ".jpeg")):
             shutil.copy2(f"article/{filename}", "/app")
+
+def construct_pandoc(
+        *,
+        standalone=True,
+        lua_filters=[],
+        bibliography_processing="citeproc",
+        bibliography_csl_style=None,
+        bibliography_biblatex_options=[],
+        bibliography_file=None,
+        number_sections=True,
+        pdf_engine=None,
+        pdf_engine_options = {
+            "latexmk": ["-lualatex", "-diagnostics"]
+        },
+        html_options = [],
+        from_format="markdown",
+        template=None,
+        proofs=False,
+        automatic_widow_removal=False,
+        input_metadata=None,
+        input_file=None,
+        input_journal_metadata=None,
+        input_bibliography_metadata_file=False,
+        output_format=None,
+        output_file=None,
+        ):
+    """
+    Build options for the command based on user options.
+    """
+    a = ["pandoc"]
+    if standalone:
+        a.append("-s")
+
+    try:
+        for f in lua_filters:
+            a.append(f"--lua-filter=filter/{f}")
+    except TypeError:
+        pass
+    
+    if bibliography_processing == "citeproc":
+        a.append("--citeproc")
+        if bibliography_csl_style:
+            a.append("--csl")
+            a.append(bibliography_csl_style)
+        else:
+            raise ValueError("No CSL specified.")
+        
+    if bibliography_processing == "biblatex":
+        a.append("--biblatex")
+        for o in bibliography_biblatex_options:
+            a.append(f"-V biblatexoptions={o}")
+
+    if number_sections:
+        a.append("--number-sections")
+
+    if from_format:
+        a.append("--from")
+        a.append(from_format)
+    else:
+        raise ValueError("No source format specified.")
+    
+    if template:
+        a.append("--template")
+        a.append(template)
+    else:
+        raise ValueError("No template specified.")
+    
+    if bibliography_file:
+        a.append("--bibliography")
+        a.append(bibliography_file)
+
+    if pdf_engine:
+        a.append("--pdf-engine")
+        a.append(pdf_engine)
+
+        try:
+            # If there are options for a PDF engine, apply them.
+            for o in pdf_engine_options[pdf_engine]:
+                a.append(f"--pdf-engine-opt={o}")
+        except KeyError:
+            # Not all PDF engines need to have options.
+            pass
+
+    for o in html_options:
+        a.append(o)
+
+    if proofs:
+        a.append("-M")
+        a.append("proofs")
+
+    if automatic_widow_removal:
+        a.append("-M")
+        a.append("widows")
+
+    if input_journal_metadata:
+        a.append(input_journal_metadata)
+
+    if input_metadata:
+        a.append(input_metadata)
+    
+    if input_file:
+        a.append(input_file)
+    else:
+        raise ValueError("Input file not specified.")
+    
+    if input_bibliography_metadata_file and bibliography_file:
+        a.append("tex/bibliography-preamble.tex")
+    
+    if output_format:
+        a.append("-t")
+        a.append(output_format)
+
+    if output_file:
+        a.append("-o")
+        a.append(output_file)
+    else:
+        raise ValueError("Not output file specified.")
+    
+    return subprocess.run(a, capture_output=True, text=True)
+
+
 
 def main(args) -> None:
     '''Main function to convert the markdown file to various output formats.
@@ -138,119 +279,103 @@ def main(args) -> None:
     TEXFILE = f"{PLAINFILENAME}.tex"
     JATSFILE = f"{PLAINFILENAME}.jats"
     BIBLIOGRAPHY = f"{INBIBTEX}" if INBIBTEX else None
-    CSLFILE = "csl/apa7-single-spaced.csl" 
-    TEXTEMPLATE = "templates/phimisci-classic.tex"
+    active_filters = [i for i in args.filter]
+    if args.compounds:
+        active_filters.append("latex-compound-words.lua")
+
+    if args.parentheses:
+        CSLFILE = "csl/apa7-manual-parentheses.csl" 
+    else:
+        CSLFILE = "csl/apa7-single-spaced.csl" 
+
+    if args.layout == "classic":
+        TEXTEMPLATE = "templates/phimisci-classic.tex"
+    if args.layout == "twocolumn":
+        TEXTEMPLATE = "templates/phimisci-twocolumn.tex"
 
     copy_files_to_app_dir()
 
-    # PDF generation
+    if args.layout == "classic":
+        base_config = {
+            "lua_filters": active_filters,
+            "bibliography_processing": "citeproc",
+            "bibliography_csl_style": CSLFILE,
+            "pdf_engine": "xelatex",
+            "bibliography_file": BIBLIOGRAPHY,
+            "template": TEXTEMPLATE,
+            "input_journal_metadata": "templates/MMM_JOURNAL_METADATA.yaml",
+            "input_metadata": INMETADATA,
+            "input_file": INMARKDOWN,
+            "input_bibliography_metadata_file": True,
+        }
+    
+    if args.layout=="twocolumn":
+        base_config = {
+            "lua_filters": active_filters,
+            "bibliography_processing": "biblatex",
+            "pdf_engine": "latexmk",
+            "bibliography_file": BIBLIOGRAPHY,
+            "template": TEXTEMPLATE,
+            "input_metadata": INMETADATA,
+            "input_file": INMARKDOWN,
+            "automatic_widow_removal": args.widows
+        }
+
+    # PUBLICATION PDF
     if args.pdf:
-        PDF_BASE_COMMAND = [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, 
-            "--bibliography", BIBLIOGRAPHY, "--csl", CSLFILE,
-            "templates/MMM_JOURNAL_METADATA.yaml", INMETADATA, INMARKDOWN, 
-            "tex/bibliography-preamble.tex", "-o", PDFFILE
-        ] if BIBLIOGRAPHY else [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, "--csl", CSLFILE, 
-            "templates/MMM_JOURNAL_METADATA.yaml",
-            INMETADATA, INMARKDOWN, "-o", PDFFILE
-        ]
-        if args.filter:
-            for filter in reversed(args.filter):
-                PDF_BASE_COMMAND.insert(2, f"--lua-filter=filter/{filter}")
-        result = subprocess.run(PDF_BASE_COMMAND, capture_output=True, text=True)
+        result = construct_pandoc(
+            **base_config, 
+            output_file=PDFFILE
+            )
         logging(LOGFILE, result)
 
-    # PDF proof generation
+    # PROOFS
     if args.proof:
-        PDF_BASE_COMMAND = [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, "--bibliography", BIBLIOGRAPHY, "--csl", CSLFILE,
-            "templates/MMM_JOURNAL_METADATA.yaml", "-V proofs=1", 
-            "--include-in-header=tex/proofs.tex", 
-            INMETADATA, INMARKDOWN, "tex/bibliography-preamble.tex", "-o", 
-            PROOFFILE
-        ] if BIBLIOGRAPHY else [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, "--csl", CSLFILE, 
-            "templates/MMM_JOURNAL_METADATA.yaml", 
-            "-V proofs=1", "--include-in-header=tex/proofs.tex", 
-            INMETADATA, INMARKDOWN, "-o", PROOFFILE
-        ]
-        if args.filter:
-            for filter in reversed(args.filter):
-                PDF_BASE_COMMAND.insert(2, f"--lua-filter=filter/{filter}")
-        result = subprocess.run(PDF_BASE_COMMAND, capture_output=True, text=True)
+        result = construct_pandoc(
+            **base_config,
+            proofs=True,
+            output_file=PROOFFILE
+            )
         logging(LOGFILE, result)
-    
-    # LaTeX generation
+
+    # TEX SOURCE FILE
     if args.tex:
-        TEX_COMMAND = [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, "--bibliography", BIBLIOGRAPHY, 
-            "--csl", CSLFILE,
-            "templates/MMM_JOURNAL_METADATA.yaml", 
-            INMETADATA, INMARKDOWN, "tex/bibliography-preamble.tex", "-o", TEXFILE
-        ] if BIBLIOGRAPHY else [
-            "pandoc", "-s", "--citeproc", "--number-sections", 
-            "--pdf-engine=xelatex", "--from", "markdown",
-            "--template", TEXTEMPLATE, "--csl", CSLFILE, 
-            "templates/MMM_JOURNAL_METADATA.yaml",
-            INMETADATA, INMARKDOWN, "-o", TEXFILE
-        ]
-        if args.filter:
-            for filter in reversed(args.filter):
-                TEX_COMMAND.insert(1, f"--lua-filter=filter/{filter}")
-        result = subprocess.run(TEX_COMMAND, capture_output=True, text=True)
+        result = construct_pandoc(
+            **base_config,
+            output_file=TEXFILE
+            )
         logging(LOGFILE, result)
+
 
     # JATS generation
     if args.jats:
-        JATS_COMMAND = [
-            "pandoc", "--citeproc", "--number-sections", "-s", "-N", 
-            "--from", "markdown",
-            "--bibliography", BIBLIOGRAPHY, "templates/MMM_JOURNAL_METADATA.yaml", 
-            INMETADATA, INMARKDOWN,
-            "-t", "jats+element_citations", "-o", JATSFILE
-        ] if BIBLIOGRAPHY else [
-            "pandoc", "--citeproc", "--number-sections", "-s", "-N", "--from", 
-            "markdown",
-            "templates/MMM_JOURNAL_METADATA.yaml", INMETADATA, INMARKDOWN, 
-            "-t", "jats+element_citations", "-o", JATSFILE
-        ]
-        if args.filter:
-            for filter in reversed(args.filter):
-                JATS_COMMAND.insert(1, f"--lua-filter=filter/{filter}")
-        result = subprocess.run(JATS_COMMAND, capture_output=True, text=True)
+        result = construct_pandoc(
+            lua_filters=active_filters,
+            bibliography_processing="citeproc",
+            bibliography_file=BIBLIOGRAPHY,
+            input_journal_metadata="templates/MMM_JOURNAL_METADATA.yaml",                    
+            input_metadata=INMETADATA,
+            input_file=INMARKDOWN,
+            output_format="jats+element_citations",
+            output_file=JATSFILE
+        )
         logging(LOGFILE, result)
+
 
     # HTML generation
     if args.html:
-        HTML_COMMAND = [
-            "pandoc", "--citeproc", "--number-sections", "--mathjax", "--from", 
-            "markdown",
-            "--template=templates/MMM_HTML_TEMPLATE.html", 
-            "--bibliography", BIBLIOGRAPHY, "--csl", CSLFILE,
-            "templates/MMM_JOURNAL_METADATA.yaml", INMETADATA, INMARKDOWN, 
-            "-o", HTMLFILE
-        ] if BIBLIOGRAPHY else [
-            "pandoc", "--citeproc", "--number-sections", "--mathjax", 
-            "--from", "markdown",
-            "--template=templates/MMM_HTML_TEMPLATE.html", 
-            "--csl", CSLFILE, "templates/MMM_JOURNAL_METADATA.yaml",
-            INMETADATA, INMARKDOWN, "-o", HTMLFILE
-        ]
-        if args.filter:
-            for filter in reversed(args.filter):
-                HTML_COMMAND.insert(1, f"--lua-filter=filter/{filter}")
-        result = subprocess.run(HTML_COMMAND, capture_output=True, text=True)
+        result = construct_pandoc(
+            lua_filters=active_filters,
+            bibliography_processing="citeproc",
+            html_options=["--mathjax"],
+            template="templates/MMM_HTML_TEMPLATE.html",
+            bibliography_file=BIBLIOGRAPHY,
+            bibliography_csl_style=CSLFILE,
+            input_journal_metadata="templates/MMM_JOURNAL_METADATA.yaml",
+            input_metadata=INMETADATA,
+            input_file=INMARKDOWN,
+            output_file=HTMLFILE
+        )
         logging(LOGFILE, result)
 
     print("Conversion finished.")
